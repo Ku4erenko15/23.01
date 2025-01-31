dfc65d787ec7a57f85096648eb6001e9f607497b
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-01-31 14:05:31 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Иван", "Иванов", 30);
+            person.PrintInfo();
+            Console.ReadKey();
+        }
+        class Person
+        {
+            private string firstName;
+            private string lastName;
+            private int age;
+
+            // Конструктор класса
+            public Person(string firstName, string lastName, int age)
+            {
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.age = age;
+            }
+
+            // Метод для вывода информации о человеке
+            public void PrintInfo()
+            {
+                Console.WriteLine($"Имя: {firstName}");
+                Console.WriteLine($"Фамилия: {lastName}");
+                Console.WriteLine($"Возраст: {age}");
+            }
+        }
+    }
+}
+
+   
+
+
+
+
